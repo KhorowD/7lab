10afe3e2d89ac6c9f8d02377ac704e0ded5f2bda
KhorowD <danilkhorov@gmail.com>
2019-03-13 15:59:52 +0700

    Начало лабораторной работы

---
+++ 7.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+extern "C" int AllocMemory(int size, int* matrix);
+extern "C" int MinVal(int size, int *min, int* matrix);
+int main()
+{
+	int matrix = 0;
+	int size = 0;
+	int i,j;
+	int min = 0;
+	
+	printf("Enter matrix size: \n");
+	
+	
+	return 0;
+}
